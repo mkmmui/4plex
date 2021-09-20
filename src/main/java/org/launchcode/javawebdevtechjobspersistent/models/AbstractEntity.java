cea0a5478f92b38bc0d77697422ef7b481ae3283
mkmmui <80076463+mkmmui@users.noreply.github.com>
2021-09-20 13:20:58 -0500

    user registration working

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -15,8 +15,8 @@ public abstract class AbstractEntity {
 
     private int id;
 
-    @NotBlank(message = "Name required.")
-    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters.")
+//    @NotBlank(message = "Name required.")
+//    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters.")
 
     private String name;
 
